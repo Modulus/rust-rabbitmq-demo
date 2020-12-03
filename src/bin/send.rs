cdfe99f6e86109bbb29c3bcff6d70eca70689872
John Sigvald Skauge <johnsigvaldskauge@tv2.no>
2020-12-03 12:18:48 +0100

    Trying pipeline

--- src/bin/send.rs
+++ src/bin/send.rs
@@ -1,4 +1,9 @@
 use amiquip::{Connection, Exchange, Publish, Result};
+// use rand::seq::SliceRandom; // 0.7.2
+
+use std::env;
+use names::{Generator, Name};
+use std::{thread, time};
 
 
 // use serde::Deserialize;
@@ -22,23 +27,49 @@ use amiquip::{Connection, Exchange, Publish, Result};
 //
 //
 // }
-
-
-
 fn main() -> Result<()> {
-    // Open connection.
-    let mut connection = Connection::insecure_open("amqp://kanin:kanin@localhost:5672")?;
 
-    // Open a channel - None says let the library choose the channel ID.
-    let channel = connection.open_channel(None)?;
+    let rabbitmq_url : String = env::var("RABBITMQ_CONNECTION_STRING").unwrap_or("amqp://kanin:kanin@localhost:5672".to_string());
 
-    // Get a handle to the direct exchange on our channel.
-    let exchange = Exchange::direct(&channel);
+    println!("Calling rabbitmq on {:?}", rabbitmq_url);
 
-    // Publish a message to the "hello" queue.
-    exchange.publish(Publish::new("hello there".as_bytes(), "hello"))?;
+    let mut generator = Generator::with_naming(Name::Numbered);
 
-    connection.close()
+    // Routing keys
+    // let routing_keys = ["", "hello", "hi", "random", "1239213", "asdfg"];
+
+    loop {
+        // Open connection.
+
+        let mut connection = Connection::insecure_open(&rabbitmq_url)?;
+
+        // Open a channel - None says let the library choose the channel ID.
+        let channel = connection.open_channel(None)?;
+
+        // Get a handle to the direct exchange on our channel.
+        let exchange = Exchange::direct(&channel);
+
+        let name = generator.next().unwrap();
+        println!("current name is: {}", name);
+
+        // let routing_key: Vec<_> = routing_keys
+        //     .choose_multiple(&mut rand::thread_rng(), 1)
+        //     .collect();
+        // println!("Current routing key{:?}", routing_key);
+        //
+        // Publish a message to the "hello" queue.
+        exchange.publish(Publish::new(name.as_bytes(), "hello"))?;
+
+        connection.close();
+        let ten_millis = time::Duration::from_secs(2);
+
+        thread::sleep(ten_millis);
+
+    }
+
+    println!("Done!");
+
+
 }
 
 
